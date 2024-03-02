03f88eb2d00f340e087384070e358c9f501ca5f1
saravanaguhan19 <saravanaguhan19@gmail.com>
2024-03-02 16:37:15 +0530

    added client side routing using react-router-dom

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,11 +1,59 @@
 import Avatar from "@mui/material/Avatar";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 
 export default function UserDetails() {
-  return (
-    <Avatar
-      alt="Remy Sharp"
-      src="https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg"
-      sx={{ width: 160, height: 160 }}
-    />
-  );
+  let { id } = useParams();
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`https://dummyapi.io/data/v1/user/${id}`, {
+        headers: { "app-id": import.meta.env.VITE_APP_ID },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setUser({ ...response.data });
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <Card
+        sx={{ display: "flex", width: "600px", margin: "0 auto", mt: "3rem" }}
+      >
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <CardContent sx={{ flex: "1 0 auto" }}>
+            <Typography component="div" variant="h5">
+              {user.firstName + " " + user.lastName}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              {user.email}
+            </Typography>
+          </CardContent>
+        </Box>
+        <CardMedia
+          component="img"
+          sx={{ width: 151 }}
+          image={user.picture}
+          alt={user.firstName}
+        />
+      </Card>
+    );
+  }
 }
