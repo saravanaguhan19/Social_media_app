4c7282f8ce843db305b32084902db723b668f966
saravanaguhan19 <saravanaguhan19@gmail.com>
2024-03-02 17:26:17 +0530

    implemented custom hook for getting post and code cleanup done

--- src/components/SocialApp.jsx
+++ src/components/SocialApp.jsx
@@ -1,30 +1,15 @@
 import Input from "./Input/Input";
 import MainContainer from "./MainContainer/MainContainer";
-import Navbar from "./Navbar/Navbar";
-import PostCardContext from "../providers/PostProvider";
-import { useState, useEffect } from "react";
-import axios from "axios";
-function SocialApp() {
-  const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    console.log(import.meta.env.VITE_APP_ID);
-    //we will download the content form dummy api
-    axios
-      .get("https://dummyapi.io/data/v1/post", {
-        headers: { "app-id": import.meta.env.VITE_APP_ID },
-      })
-      .then((response) => {
-        const responseObject = response.data;
-        console.log(response.data.data);
-        setPosts([...responseObject.data]);
-      });
-  }, []);
+import PostCardContext from "../providers/PostProvider";
+import usePosts from "../hooks/usePosts";
+
+function SocialApp() {
+  const { posts, setPosts } = usePosts();
 
   return (
     <div>
       <PostCardContext.Provider value={{ posts, setPosts }}>
-        
         <Input />
         <MainContainer />
       </PostCardContext.Provider>
--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,3 +1,22 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function usePosts(){
-    
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+      console.log(import.meta.env.VITE_APP_ID);
+      //we will download the content form dummy api
+      axios
+        .get("https://dummyapi.io/data/v1/post", {
+          headers: { "app-id": import.meta.env.VITE_APP_ID },
+        })
+        .then((response) => {
+          const responseObject = response.data;
+          console.log(response.data.data);
+          setPosts([...responseObject.data]);
+        });
+    }, []);
+
+    return {posts ,setPosts };
 }
